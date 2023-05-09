72646ca110b90a1fc1feaca24f2aad54686aace0
Biplav-00 <biplav.ghimire@truenary.com>
2023-05-09 15:48:40 +0545

    admin dashboard customized and other remaining task is done

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question,Choice
 
-admin.site.register(Question)
+#admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra=3
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields =["pub_date","question_text"]
+    #customize the question section only
+    list_display = ["question_text","pub_date"]
+    list_filter=["pub_date"]
+    search_fields=["question_text"]
+    fieldsets=[
+    ("Question Section",{"fields":["question_text"]}),
+    ("Date Information",{"fields":["pub_date"],
+                         "classes":["collapse"]},)
+    ]
+    inlines=[ChoiceInline]
+    #list_display = ["question_text","pub_date"]
+    
+    
+admin.site.register(Question,QuestionAdmin)
+#admin.site.register(Choice)
